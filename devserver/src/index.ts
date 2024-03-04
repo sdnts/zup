728927177d7c00a1a1f0755682efea18dfdaf584
Siddhant <git@sdnts.dev>
2024-03-03 22:14:32 -0600

    improve(devserver): mirror real upstream paths

--- devserver/src/index.ts
+++ devserver/src/index.ts
@@ -1,28 +1,41 @@
-const server = Bun.serve({
-  port: 3000,
+const zig = Bun.serve({
+  port: 8000,
   async fetch(request) {
     const url = new URL(request.url);
-    console.log("Incoming request", url.pathname);
+    console.log("(zig) Incoming request", url.pathname);
 
-    if (url.pathname === "/zig/index.json") {
+    if (url.pathname === "/download/index.json") {
       return new Response(await Bun.file("src/zig.json").text(), {
         headers: { "Content-Type": "application/json" },
       });
     }
-    if (url.pathname === "/zig") {
+
+    if (url.pathname.startsWith("/builds/")) {
       return new Response(Bun.file("src/zig.tar.xz"));
     }
+
+    return new Response("Bad Request", { status: 400 });
+  },
+});
+console.log(`(zig) Serving on ${zig.port}`);
+
+const zls = Bun.serve({
+  port: 9000,
+  async fetch(request) {
+    const url = new URL(request.url);
+    console.log("(zls) Incoming request", url.pathname);
 
     if (url.pathname === "/zls/index.json") {
       return new Response(await Bun.file("src/zls.json").text(), {
         headers: { "Content-Type": "application/json" },
       });
     }
-    if (url.pathname === "/zls") {
+
+    if (url.pathname.startsWith("/zls/")) {
       return new Response(Bun.file("src/zls"));
     }
 
-    return new Response("ok");
+    return new Response("Bad Request", { status: 400 });
   },
 });
-console.log(`Running on ${server.port}`);
+console.log(`(zls) Serving on ${zls.port}`);
