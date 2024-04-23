9be93e3b043e58764951055c5ae244e0cfdb4a35
Siddhant <git@sdnts.dev>
2024-04-22 21:45:41 -0500

    chore: bump version to 0.1.0

--- src/zup.zig
+++ src/zup.zig
@@ -20,5 +20,5 @@ pub fn help() !void {
 
 pub fn version() !void {
     const stdout = std.io.getStdOut();
-    stdout.writeAll("0.0.1") catch unreachable;
+    stdout.writeAll("0.1.0") catch unreachable;
 }
